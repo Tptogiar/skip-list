4acd9db6e22f7c4c6072e521f6361e7d97c876ee
Tptogiar <2528891112@qq.com>
2022-09-07 01:25:58 +0800

    feat(api): add SkipList api

--- SkipList.h
+++ SkipList.h
@@ -5,4 +5,26 @@
 #ifndef SKIP_LIST_SKIPLIST_H
 #define SKIP_LIST_SKIPLIST_H
 
+struct SkipListNode{
+    int key;
+    int value;
+    int maxLevel;
+    struct SkipListNode* next[];
+};
+
+struct SkipList{
+    int level;
+    int nodeCount;
+    struct SkipListNode* head;
+};
+
+extern struct SkipList* skipListCreate(int maxLevel);
+extern int skipListInsert(struct SkipList* list,int key,int value);
+extern int skipListDelete(struct SkipList* list,int key);
+extern int skipListModify(struct SkipList* list,int key,int value);
+extern int skipListSearch(struct SkipList* list,int key,int *value);
+extern int skipListDestroy(struct SkipList* list);
+
+
+
 #endif //SKIP_LIST_SKIPLIST_H
